c5ab87bb12e90ee2c1f1a81ee85d5fc04080cfd1
jian0209 <95278074+jian0209@users.noreply.github.com>
2024-01-24 10:17:53 +0800

    fix bug

--- script.sh
+++ script.sh
@@ -15,20 +15,21 @@ Help()
     echo "# Install: sudo apt-get install pv #"
     echo "####################################"
     echo ""
-    echo "Syntax: $0 [-h|-H|-P|--output|--databases|--no-data|...]> < password.txt"
+    echo "Syntax: $0 [-h|-H|-P|--output|--databases|--no-data|...] < password.txt"
     echo "Syntax: $0 [-h|-H|-P|--output|--databases|--no-data|...]"
-    echo "Usage: $0 -H=\"localhost\" -P=\"3306\" --output=\"/tmp/output.sql.gz\" --databases=\"database1 database2\" --no-data <user> (<password> | < password.txt)"
+    echo "Usage: $0 -H=\"localhost\" -P=\"3306\" --output=\"/tmp/output.sql.gz\" --databases=\"database1 database2\" --no-data < password.txt"
     echo ""
     echo "options:"
-    echo "-h --help     Print this Help."
-    echo "-H --host     Hostname or IP address of the database server.[Default: localhost]"
-    echo "-u --user     Username of the database server.[Default: root]"
-    echo "-P --port     Port number of the database server.[Default: 3306]"
-    echo "--output      Output file path, will using gzip to store the file with .gz extension, recommended extension: '.sql'.[Default: Output to STDOUT]"
-    echo "--where       Dump only rows selected by given WHERE condition. [Default: Dump all rows]"
-    echo "--databases   Dump several databases. Give the database names separated by space. [Default: Dump all databases]"
-    echo "--decryption  Decrypt the input file using base64. [Default: No]"
-    echo "--no-data     Do not dump table contents. [Default: No]"
+    echo "-h --help           Print this Help."
+    echo "-H --host           Hostname or IP address of the database server.[Default: localhost]"
+    echo "-u --user           Username of the database server.[Default: root]"
+    echo "-P --port           Port number of the database server.[Default: 3306]"
+    echo "--output            Output file path, will using gzip to store the file with .gz extension, recommended extension: '.sql'.[Default: Output to /tmp/output.sql.gz]"
+    echo "--where             Dump only rows selected by given WHERE condition. [Default: Dump all rows]"
+    echo "--databases         Dump several databases. Give the database names separated by space. [Default: Dump all databases]"
+    echo "--decryption        Decrypt the input file using base64. [Default: No]"
+    echo "--no-data           Do not dump table contents. [Default: No]"
+    echo "--skip-lock-tables  Do not lock tables for backup. [Default: No]"
 }
 
 #################
@@ -36,6 +37,7 @@ Help()
 #################
 
 user="root"
+password=""
 databases=""
 host="localhost"
 port="3306"
@@ -85,6 +87,9 @@ fi
             --no-data)
                 no_data=true
             ;;
+            --skip-lock-tables)
+                skip_lock_tables=true
+            ;;
             *)
             ;;
         esac
@@ -119,6 +124,10 @@ fi
     if [ "$no_data" = true ]; then
         execute_dump_command="$execute_dump_command --no-data"
     fi
+
+    if [ "$skip_lock_tables" = true ]; then
+        execute_dump_command="$execute_dump_command --skip-lock-tables"
+    fi
     
     if [ "$where" != "" ]; then
         databases_length=$(echo $databases | wc -w)
@@ -133,28 +142,28 @@ fi
     if [ "$databases" != "" ]; then
         execute_dump_command="$execute_dump_command --databases $databases"
         databases=$(echo $databases | sed 's/ /","/g' | sed 's/^/"/' | sed 's/$/"/')
-        execute_get_size_command=$(mysql -h$host -uroot -p$password -e "SELECT ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS 'Size (MB)' FROM information_schema.TABLES WHERE table_schema IN ($databases) GROUP BY table_schema;" | tail -n +2 | grep -v -E '(information_schema|performance_schema|mysql|sys|innodb|tmp)' | awk '{s+=$1} END {print s}')
+        execute_get_size_command=$(mysql -h$host -u$user -p$password -e "SELECT ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS 'Size (MB)' FROM information_schema.TABLES WHERE table_schema IN ($databases) GROUP BY table_schema;" | tail -n +2 | grep -v -E '(information_schema|performance_schema|mysql|sys|innodb|tmp)' | awk '{s+=$1} END {print s}')
     fi
     
     if [ "$has_database" = false ]; then
         execute_dump_command="$execute_dump_command ""--databases"" $database_list"
     fi
     
-    file_size=$execute_get_size_command
+    file_size=$(echo $execute_get_size_command | awk '{print int($1+0.5)}')
 
     if [ "$output_file" != "" ]; then
         execute_dump_command="$execute_dump_command | pv -W -s $file_size""M | gzip -c > $output_file"
     else
-        execute_dump_command="$execute_dump_command | pv -W -s $file_size""M | gzip -c > /dev/null"
+        execute_dump_command="$execute_dump_command | pv -W -s $file_size""M | gzip -c > /tmp/output.sql.gz"
     fi
-    
+
     echo "Done Configuration"
 
     ################
     # Main Program #
     ################
     echo "Start Dumping"
-    echo $execute_dump_command
+    echo "Total File Size Around: $file_size""M"
     eval $execute_dump_command
     echo "Done Dumping"
 ) || exit 1
